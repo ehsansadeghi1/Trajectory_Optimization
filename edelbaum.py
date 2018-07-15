368f5c6f72bf46e069846b4ecadeeffdbb1457a0
ehsansadeghi1 <36340524+ehsansadeghi1@users.noreply.github.com>
2018-07-15 14:15:19 +0200

    Create edelbaum.py

---
+++ edelbaum.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Edelbaum theory, reformulated by Kéchichian.
+References
+----------
+* Edelbaum, T. N. "Propulsion Requirements for Controllable
+  Satellites", 1961.
+* Kéchichian, J. A. "Reformulation of Edelbaum's Low-Thrust
+  Transfer Problem Using Optimal Control Theory", 1997.
+"""
+from math import atan2, atan,tan, sin, cos, pi, sqrt, radians, degrees
+import matplotlib.pyplot as plt
+mu = 398600.4415  # earth gravitational constant (km^3/sec^2)
+r_eq = 6378.136   # earth equatorial radius (kilometers)
+
+h_0 = 7000-r_eq
+h_f = 42166-r_eq
+inc_0 = 28.5
+inc_f = 0
+f = 3.5e-7  # km/sec
+
+inc_0 = radians(inc_0)
+inc_f = radians(inc_f)
+r_0 = r_eq + h_0
+r_f = r_eq + h_f
+V_0 = sqrt(mu/r_0)
+V_f = sqrt(mu/r_f)
+d_inc = abs(inc_f-inc_0)
+"initial yaw angle"
+beta0 = atan2(sin(0.5*pi*d_inc), (V_0/V_f)-cos(0.5*pi*d_inc))
+"Total delta_V"
+dV_t = V_0*cos(beta0)-V_0*sin(beta0)/tan(0.5*pi*d_inc+beta0)
+"Flight Time"
+f_time = dV_t/f
+if f_time < 3600:
+    # minutes
+    tdflag = 1
+    f_time = f_time/60
+    print(f_time, "Minutes")
+elif f_time < 86400:
+    # Hours
+    tdflag = 2
+    f_time = f_time/3600
+    print(f_time, "Hours")
+else:
+    # Days
+    tdflag = 3
+    f_time = f_time/86400
+    print(f_time, "Days")
+dt = f_time/100
+t_ = -dt
+t = []
+beta = []
+V_ = []
+a_ = []
+i_ = []
+for i in range(0,101):
+    t_ += dt
+    if tdflag == 1:
+        tsec = t_*60
+    elif tdflag == 2:
+        tsec = t_*3600
+    else:
+        tsec = t_*86400
+    t.append(t_)
+    beta.append(degrees(atan2(V_0*sin(beta0), (V_f*cos(beta0)-f*tsec))))
+    V = (sqrt(V_0*V_0-2*V_0*f*tsec*cos(beta0)+f*f*tsec*tsec))
+    a = (mu / (V * V))/1000
+    tmp1 = atan((f * tsec - V_0 * cos(beta0))/(V_0 * sin(beta0)))
+    dinc = degrees((2/pi)*(tmp1 + 0.5 * pi - beta0))
+    i = degrees(inc_0)-dinc
+    V_.append(V)
+    a_.append(a)
+    i_.append(i)
+
+fig, ax1 = plt.subplots()
+ax1.plot(t, i_, 'b-')
+ax1.set_xlabel('Time')
+ax1.set_ylabel('Inclination (Degrees)', color='b')
+#ax1.tick_params('y', colors='b')
+
+ax2 = ax1.twinx()
+ax2.plot(t, V_, 'r.')
+ax2.set_ylabel('Velocity', color='r')
+#ax2.tick_params('y', colors='r')
+
+plt.show()
